c239b1459997ec3d78a188f82a310234a7b17b7d
Mohga <mogatorkey@yahoo.com>
2023-02-16 13:22:38 -0800

    fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+
+/*main - Entry
+ *
+ * description 'don't use printf'
+ *
+ * return zero
+ * */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts ("Programming is like building a multilingual puzzle");
 	return(0);
 }
