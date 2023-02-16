048a7bd884d9a7bc08eafc6ff053dab6ad4c6357
Mohga <mogatorkey@yahoo.com>
2023-02-16 14:11:02 -0800

    print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*main - write a line using printf
+ *
+ * Reatuen: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+i		return (0);
+}
