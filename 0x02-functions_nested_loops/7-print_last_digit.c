6f12b77a8b6545016811d871dd7257ca508a728e
Mohga <mogatorkey@yahoo.com>
2023-02-21 13:58:04 -0800

    fix

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - function that prints the last digit of a number.
+ *
+ * Return: ld
+ *
+ * @n: parameter to look at.
+ */
+
+int print_last_digit(int n)
+{
+	int ld;
+	ld = n % 10;
+	if (n < 0)
+	{
+		ld = -ld;
+	}
+	return (ld);
+}
