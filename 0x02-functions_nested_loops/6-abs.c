9af9aa4d5f0df39b069d8dfe7fd325bbff054d9a
Mohga <mogatorkey@yahoo.com>
2023-02-21 13:41:29 -0800

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - checks for absoloute value.
+ *
+ * @a: parameter to be checked.
+ *
+ * Return: a
+ */
+
+int _abs(int)
+{
+	if (a < 0)
+	{
+		a = -(a);
+	}
+	else if (a >= 0)
+	{
+		a = a;
+	}
+	return (a);
+}
