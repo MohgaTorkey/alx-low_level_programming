9f9c14fcc126cecf4360b92a2f38e21b60497e47
Mohga <mogatorkey@yahoo.com>
2023-02-21 12:08:55 -0800

    fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,10 +7,8 @@
  * return: 0.
  */
 
-int main(void)
+void print_alphabet_x10(void)
 {
-	void print_alphabet_x10(void)
-	{
 	int i;
 	char j;
 
@@ -22,6 +20,4 @@ int main(void)
 		}
 		_putchar('\n');
 	}
-	}
-	return (0);
 }
