1431dc79fba11670525384a1ea3444246146ad4c
Mohga <mogatorkey@yahoo.com>
2023-02-21 15:03:59 -0800

    fix

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -7,13 +7,28 @@
 
 void times_table(void)
 {
-	int number = 9;
-	int count = 1;
-	
-	while (count <= 10)
+	int num, mult, prod;
+
+	for (num = 0; num <= 9; num++)
 	{
-		int product = (number * count);
-		_putchar(number '*' count '=' product);
-		count = count + 1;
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+			prod = num * mult;
+			
+			if (prod <= 9)
+			{
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar((prod / 10) + '0');
+			}
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
 	}
 }
