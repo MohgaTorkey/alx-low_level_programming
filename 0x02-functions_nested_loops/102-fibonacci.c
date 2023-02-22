2db2a73e7bfcdc0299742e09cf6174b595fba1c1
Mohga <mogatorkey@yahoo.com>
2023-02-21 16:51:31 -0800

    fix

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - print the first 52 fibonacci numbers
+ * Return: Nothing!
+ */
+
+int main(void)
+{
+	int i = 0;
+	long j = 1, K =2;
+
+	while (i < 50)
+	{
+		if (i == 0)
+		{
+			printf("%d", j);
+		}
+		else if (i == 1)
+		{
+			printf("%d", k);
+		}
+		else
+		{
+			k += j;
+			j = k - j;
+			printf(", %d", k);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
