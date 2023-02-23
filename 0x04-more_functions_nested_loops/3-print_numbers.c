92457aade86587f78fe59449f1e4d8aa7a19ec6a
Mohga <mogatorkey@yahoo.com>
2023-02-23 11:08:45 -0800

    fix

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,7 +4,6 @@
 /**
  * print_numbers - prints the numbers, from 0 to 9
  *
- * Return: 0 
  */
 
 void print_numbers(void)
@@ -16,5 +15,4 @@ void print_numbers(void)
 		_putchar(i);
 		_putchar('\n');
 	}
-	return (0);
 }
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -1,22 +1,14 @@
-#include "main.h"
+#include <unistd.h>
 
 /**
- * main - program that writes _putchar.
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: 0
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 
-int main(void)
+int _putchar(char c)
 {
-	_putchar('-');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
-
-	return (0);
+	return (write(1, &c, 1));
 }
