3462077db50f4ff566a51f1a81bbd2fde184b911
Mohga <mogatorkey@yahoo.com>
2023-02-23 09:34:33 -0800

    fix

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _isupper - function that checks for uppercase character.
+ *
+ * Return: 1 if c is uppercase and 0 if not.
+ */
+
+int main(void)
+{
+	char c;
+
+	int _isupper(int c);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,7 @@
+#ifndef main_H_  
+#define main_H_
+
+int _isupper(int c);
+
+
+#endif // main_H_
