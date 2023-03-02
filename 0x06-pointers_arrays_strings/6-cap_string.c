d40b249a2ea3b3d68d971ee61c3ca78eb1670784
Mohga <mogatorkey@yahoo.com>
2023-03-02 07:08:59 -0800

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,9 +14,8 @@ char *cap_string(char *str)
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-		{
 			index++;
-			if (
+		if (
 				str[index - 1] == ' ' ||
 				str[index - 1] == '\t' ||
 				str[index - 1] == '\n' ||
@@ -31,11 +30,8 @@ char *cap_string(char *str)
 				str[index - 1] == '{' ||
 				str[index - 1] == '}' ||
 				index == 0)
-		{
-			str[index] -= 32;
-		}
-		}
-		index++;
+				str[index] -= 32;
+			index++;
 	}
 	return (str);
 }
