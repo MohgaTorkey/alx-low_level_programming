61f2bab8d43681430520c0d9ed4c8bea26153e31
Mohga <mogatorkey@yahoo.com>
2023-03-02 09:59:39 -0800

    fix

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,4 +1,8 @@
 #include "main.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
  * rot13 -  function that encodes a string using rot13
@@ -8,34 +12,3 @@
  */
 char *rot13(char *src)
 {
-	if(src == NULL)
-	{
-		return NULL;
-	}
-
-	char* result = malloc(strlen(src));
-
-	if (result != NULL)
-	{
-		strcpy(result, src);
-		char* current_char = result;
-		
-		while(*current_char != '\0')
-		{
-			if((*current_char >= 97 && *current_char <= 122) || (*current_char >= 65 && *current_char <= 90))
-			{
-				if(*current_char > 109 || (*current_char > 77 && *current_char < 91))
-				{
-					*current_char -= 13;
-				}
-				else
-				{
-					*current_char += 13;
-				}
-			}
-			current_char++;
-		}
-	}
-	return result;
-}
-
