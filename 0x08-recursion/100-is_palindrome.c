94648fd7e7235af58eb6706bac5aaf078fe3dba1
Mohga <mogatorkey@yahoo.com>
2023-03-08 12:01:02 -0800

    palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,47 +1,49 @@
 #include "main.h"
 
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - check string palindrome
+ * @s: string to reverse
+ * Return: 1 if true otherwise 0
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+		return (1);
+	else
+		return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
 /**
  * _strlen_recursion - return string length
- * @S; string
- * Return: the string length
+ * @s: string to measure length
+ *
+ * Return: length of string
  */
 
-int is_palindrome(char *s)
+int _strlen_recursion(char *s)
 {
 	if (*s == '\0')
+		return(0);
+	return (1 + _strlen_recursion(s + 1));
+}
+/**
+ * check_pal - checks the character recursively for palindrome
+ * @s: string to check
+ * @i: iterator
+ * @len: length of string
+ *
+ * Return: 1 if palindrome else 0
+ */
+
+int check_pal(char *s, int i, int len)
+{
+	if (*(s + i) != *(s + len -1))
 		return (0);
-	else
-		return (1 + _strlen_recursion(s + 1));
-}
-
-/**
- * comparator - compares string characters
- * @s: string
- * @n1: smallest iterator
- * @n2: largest iterator
- * Return: nothing
- */
-
-int comparator(char *s, int n1, int n2)
-{
-	if (*(s + n1) == *(s + n2))
-	{
-		if (n1 == n2 || n1 == n2 + 1)
-			return (1);
-		return (0 + comparator(s, n1 + 1, n2 - 1));
-	}
-	return (0);
-}
-
-/**
- * is_palindrome - detects palindrome
- * @s: string
- * Return: 1 if s is a palindrome, otherwise 0
- */
-
-int is_palindrome(char *s)
-{
-	if (*s == '\0')
+	if (i >= len)
 		return (1);
-	return (comparator(s, 0, _strlen_recursion(s) - 1));
+	return (check_pal(s, i + 1, len - 1));
 }
