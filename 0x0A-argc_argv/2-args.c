ce397662002048b9e0c0ee6d29bdd55588684dfb
Mohga <mogatorkey@yahoo.com>
2023-03-10 10:09:29 -0800

    agrs

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,14 +3,14 @@
 /**
  * main - print name of program
  * @agrc: count arguments
- * @arv: arry of argumrnts
+ * @argv: arry of argumrnts
  *
  * Return: 0
  */
 
 int main(int agrc, char *argv[])
 {
-	int counr =0;
+	int counr = 0;
 
 	if (argc > 0)
 	{
