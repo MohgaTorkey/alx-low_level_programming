4b1675f674813a6b3ab998bd6d57545d2d07e9a6
Mohga <mogatorkey@yahoo.com>
2023-03-10 10:40:18 -0800

    add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+#include <type.h>
+#include <string.h>
+
+/**
+ * check_num - check - string there are digit
+ * @str: array str
+ *
+ * Return: 0
+ */
+
+int check_num(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (! isdigit(str[count]))
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
