ad4541df0a5779778e9ce036d2ed908dd44a992e
Mohga <mogatorkey@yahoo.com>
2023-03-10 11:41:47 -0800

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -12,7 +12,7 @@
 int main(int argc, char *argv[])
 {
 	int position, total, change, aux;
-	int coins[] = {25, 18, 5, 2, 1};
+	int coins[] = {25, 10, 5, 2, 1};
 
 	position = total = change = aux = 0;
 
