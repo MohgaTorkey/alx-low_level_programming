ae030d15460b8c742bf40f69ff626995dc6cf067
Mohga <mogatorkey@yahoo.com>
2023-03-10 10:54:18 -0800

    add

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -18,11 +18,43 @@ int check_num(char *str)
 	count = 0;
 	while (count < strlen(str))
 	{
-		if (! isdigit(str[count]))
+		if (!isdigit(str[count]))
 		{
 			return (0);
 		}
 		count++;
 	}
 	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: number arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+		if (check_num(argv[count]))
+		{
+			str_to_int = atoi(argv[count]);
+			sum += str_to_int;
+		}
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+		count++;
+	}
+	return (0);
 }
