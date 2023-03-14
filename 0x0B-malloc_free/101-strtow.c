a79cefb81e229245c3c3d7a89590653b15715947
Mohga <mogatorkey@yahoo.com>
2023-03-14 13:52:31 -0700

    fix

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -36,6 +36,9 @@ int count_words(char *str)
 {
 	int index = 0, words = 0, len = 0;
 
+	for (index = 0; *(str + index); index++)
+		len++;
+
 	for (index = 0; index < len; index++)
 	{
 		if (*(str + index) != ' ')
