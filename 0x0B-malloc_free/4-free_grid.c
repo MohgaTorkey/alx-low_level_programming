33b97136a64610d2ca5e1b7a0dead9b4ddfcab1c
Mohga <mogatorkey@yahoo.com>
2023-03-14 12:57:11 -0700

    fix

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -5,7 +5,7 @@
 /**
  * free_grid - function that frees a 2 dimensional grid
  * previously created by your alloc_grid function
- * @**grid: grid provided
+ * @grid: grid provided
  * @height: height of array
  * Return: grid
  */
