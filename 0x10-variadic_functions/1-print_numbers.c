fb1ec5a7ae827eac9624f300da3d899b86da8299
Mohga <mogatorkey@yahoo.com>
2023-03-24 08:51:41 -0700

    num

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -8,7 +8,7 @@
  * @n: number of integers in function
  * @...: variable of parameters
  */
-void print_numbers(const char *separator, cont unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list nums;
 	unsigned int index;
