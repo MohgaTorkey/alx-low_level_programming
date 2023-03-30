1c39ae669003b5961c68050c539deb72b39f8914
Mohga <mogatorkey@yahoo.com>
2023-03-30 12:30:10 -0700

    free

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,17 @@
 #include <stdlib.h>
-#include "lists.h"
+#include "list.h"
 
 /**
  * free_list - free a list
  * @head: head of linked list
- * Retur: nothing
+ * Return: nothing
  */
 
 void free_list(list_t *head)
 {
 	list_t *current_node;
 
-	while ((cureent_node = head) != NULL)
+	while ((current_node = head) != NULL)
 	{
 		head = head->next;
 		free(current_node->str);
