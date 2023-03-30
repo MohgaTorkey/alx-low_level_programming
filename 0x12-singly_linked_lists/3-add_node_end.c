5971e77c0a038cb38a3c190b67b5f079a26b1a4a
Mohga <mogatorkey@yahoo.com>
2023-03-30 12:08:33 -0700

    add

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -11,7 +11,7 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *add_node_, *current_node;
+	list_t *new_node, *current_node;
 	size_t n;
 
 	new_node = malloc(sizeof(list_t));
