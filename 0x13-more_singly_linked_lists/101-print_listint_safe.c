f9cde1188cb9f49c27930fa39df1007aa885b797
Mohga <mogatorkey@yahoo.com>
2023-04-03 12:37:13 -0700

    safe

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -48,3 +48,38 @@ size_t looped_listint_len(const listint_t *head)
 
 	return (0);
 }
+
+/**
+ * print_listint_safe - print a listint 
+ * @head: point to list head
+ * Return: number of nodes
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t node, index = 0;
+
+	nodes = looped_listint_len(head);
+
+	if (nodes == 0)
+	{
+		for (; head != NULL; nodes++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+	}
+
+	else
+	{
+		for (index = 0; index < nodes; index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+
+	return (nodes);
+}
