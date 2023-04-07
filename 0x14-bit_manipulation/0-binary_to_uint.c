2267d4aca3739b2cdcc61396f19603ff1cde75d4
Mohga <mogatorkey@yahoo.com>
2023-04-07 11:04:14 -0700

    bi

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,25 @@
+#inclide "main.h"
+
+/**
+ * binary_to_unit - converts a binary to unsigned int
+ * @b: binary number
+ * Return: converted number
+ */
+
+unsigned int binary_to_unit(const char *b)
+{
+	int i;
+	unsigned int decVal = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		decVal = 2 * decVal + (b[i] - '0');
+	}
+
+	return (decVal);
+}
