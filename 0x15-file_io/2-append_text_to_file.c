cdb5cddb5d90406839439aac02c48b6c1a67d3b4
Mohga <mogatorkey@yahoo.com>
2023-04-09 13:05:09 -0700

    appen

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -23,7 +23,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		if (write(fd, text_content, _strlen(text_content)) == -1)
-			return (-1)
+			return (-1);
 	}
 
 	close(fd);
