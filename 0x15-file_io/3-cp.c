125f12f371da7dedc0d929a04ef685e2a2179214
Mohga <mogatorkey@yahoo.com>
2023-04-09 13:30:56 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -44,7 +44,7 @@ void copy_file(const char *src, const char *dest)
 
 	tfd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	while ((readed = read(ofd, buff, 1024))>0)
+	while ((readed = read(ofd, buff, 1024)) > 0)
 	{
 		if (write(tfd, buff, readed) != readed || tfd == -1)
 		{
